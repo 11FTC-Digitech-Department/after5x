44fd3e6f6c4f7cfd654a5f3e6a470e5a4ed130ab
EJ Sulit <mailbox@helloejsulit.com>
2026-01-08 13:45:56 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'after5-v2',
+  webDir: 'www'
+};
+
+export default config;
