69a6b1b2faf606461801ad464beabe8940522eb6
EJ Sulit <mailbox@helloejsulit.com>
2026-01-09 00:58:38 +0800

    Add global styles and enhance welcome page layout
    - Introduced global styles for app theme, including background gradients and button styles.
    - Redesigned the welcome page with a new layout featuring a logo, welcome text, and action buttons.
    - Implemented responsive design adjustments for various screen sizes.
    - Added SCSS styles for the welcome page to improve visual consistency and user experience.

--- src/app/domains/auth/pages/welcome/welcome.page.ts
+++ src/app/domains/auth/pages/welcome/welcome.page.ts
@@ -1,14 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonButton,
+  IonFooter
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.page.html',
   styleUrls: ['./welcome.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonFooter, 
+    IonContent,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonButton,
+    CommonModule,
+    FormsModule
+  ]
 })
 export class WelcomePage implements OnInit {
 
